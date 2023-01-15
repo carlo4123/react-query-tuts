50d390ece056555663d3ef17972de1d0208603a8
carlo cominguez <cominguez4123@gmail.com>
2023-01-15 23:24:30 +0800

    react-query-dev-tolls

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
 
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClient, QueryClientProvider } from "react-query";
 import { Route, Routes } from "react-router-dom";
 import Navigation from "./components/navigation";
 import Home from './pages/Home';
 import RQSuperHeroes from './pages/RQSuperHeroes';
 import SuperHeroes from './pages/SuperHeroes';
+
 function App() {
- 
+ const queryClient = new QueryClient()
 
   return (
-    <>
+
+  <QueryClientProvider client={queryClient} >
    <Navigation />
       <Routes>
         <Route path="/" element={ <Home/> } />
         <Route path="rqsuperheroes" element={ <RQSuperHeroes/>} />
         <Route path="superheroes" element={ <SuperHeroes/> } />
       </Routes>
-      </>
+      <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+ 
  
   )
 }
--- src/pages/RQSuperHeroes/index.jsx
+++ src/pages/RQSuperHeroes/index.jsx
@@ -1,8 +1,28 @@
+import axios from 'axios';
+import { useQuery } from "react-query";
+
 const RQSuperHeroes = () => {
+
+  const fetchSuperHeroes = () =>{
+  return  axios.get('http://localhost:4000/superheroes')
+  }
+
+  const {isLoading, data, isError, error} = useQuery('super-heroes', fetchSuperHeroes )
+
+  if(isLoading){
+    return <h2>Loading ....</h2>
+  }
+
+  if(isError){
+    return <h2>{error.message}</h2>
+  }
   return ( 
-    <div>
-      RQSuperHeroes
-    </div>
+    <>
+     <h2>RQSuperHeroe</h2> 
+     {data?.data.map((hero,index) =>{
+      return <div key={index}>{hero.name}</div>
+     })}
+    </>
    )
 }
  
